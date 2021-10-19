36a4fd32defd34ebe4aab49b51cc42f980195b6f
noobcodervj <89565075+noobcodervj@users.noreply.github.com>
2021-10-19 17:03:45 +0530

    first

---
+++ src/main/java/com/fms/FmsVjApplication.java
@@ -0,0 +1,13 @@
+package com.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsVjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsVjApplication.class, args);
+	}
+
+}
